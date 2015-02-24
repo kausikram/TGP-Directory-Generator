39f26ad921c1a8d50a0aa8c1bd85059259145afc
Kausikram Krishnasayee <kausikram@gmail.com>
2015-02-25 02:08:49 +0530

    config sample

---
+++ config.sample.py
@@ -0,0 +1,17 @@
+
+BASE_URL = "https://<subdomain>.wufoo.com/api/v3/forms/<form_name>/entries"
+COUNT_URL = BASE_URL + "/count.json"
+DATA_URL = BASE_URL + ".json"
+PARAMS = {
+    }
+
+AUTH = ('WUFOO_API_KEY', 'pass')
+
+IMAGE_FIELD = ""
+PROFILE_PICTURE_FOLDER = "./profile_pictures"
+OUTPUT_FOLDER = "./output"
+
+NAME_FIELD = ""
+EMAIL_FIELD = ""
+TWITTER_FIELD = ""
+DESCRIPTION_FIELD = ""
